092135ed6028f4087776764df4a1f5addec48f8f
Alexander Porcal <Alexander Porcal>
2023-11-30 12:23:57 -0300

    error tracker and delete old record classes

--- force-app/main/default/lwc/legalAdvisors/legalAdvisors.js
+++ force-app/main/default/lwc/legalAdvisors/legalAdvisors.js
@@ -1,17 +1,18 @@
 import { LightningElement, wire } from 'lwc';
-import getLegalAdvisorsData  from '@salesforce/apex/ucl_LegalAdvisors.getLegalAdvisorsData';
+import getLegalAdvisorsData from '@salesforce/apex/ucl_LegalAdvisors.getLegalAdvisorsData';
 
+export default class LegalAdvisorsComponent extends LightningElement {
+    legalAdvisors;
+    error;
 
-
-export default class LegalAdvisorData extends LightningElement {
-
-    @wire(getLegalAdvisorsData) LAdvisor;
-    LAdvisor({ error, data }) {
-    if (data) {
-        console.log('Legal Advisors Data:', data);
-    } else if (error) {
-        console.error('Error loading data:', error);
+    @wire(getLegalAdvisorsData)
+    wiredLegalAdvisors({ error, data }) {
+        if (data) {
+            this.legalAdvisors = data;
+    
+        } else if (error) {
+            this.error = error;
+            console.log('este es el error '+error)
+        }
     }
-}
-
 }
