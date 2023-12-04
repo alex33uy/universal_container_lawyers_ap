0f762858765ec34e803d3c24a16191abc91b4e8b
Alexander Porcal <Alexander Porcal>
2023-12-04 15:35:33 -0300

    fixing nightly update and lwc button action

--- force-app/main/default/lwc/updateButton/updateButton.js
+++ force-app/main/default/lwc/updateButton/updateButton.js
@@ -1,22 +1,34 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement } from 'lwc';
 import UpsertClients from '@salesforce/apex/ucl_UpsertClients.UpsertClients';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class updateButton extends LightningElement {
 
-    @wire (UpsertClients)
-    wiredUpsertAdvisors({error, data}){
-        if(data){
-            this.client = data;
-            alert('Lo hizo Bien');
-        }else if(error){
-            this.client = error;
-            alert('Lo hizo Mal' + error);
-            console.log(error)
-        }
-    }
+    
+    showToast(title, message, variant) {
+        const event = new ShowToastEvent({
+          title: title,
+          message: message,
+          variant: variant
+        });
+        this.dispatchEvent(event);
+      }
+    
+    
+    
     handleClick(){
-        UpsertClients();
-        alert('El button funciona')
-    }
+        UpsertClients()
+            .then(result => {
 
+                this.showToast('Success when updating data', result, 'success');
+            })
+            .catch(error => {
+                this.client = error;
+                alert('Lo hizo Mal' + error);
+                console.log(error)
+            });
+        
+        
+    }
+  
 }
