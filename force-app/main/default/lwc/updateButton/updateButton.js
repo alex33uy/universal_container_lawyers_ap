f07ce71cc5d83bbb7474ed2ea7d2ea64573967ba
Alexander Porcal <Alexander Porcal>
2023-12-04 00:32:38 -0300

    final details in creating advisors and clients

--- force-app/main/default/lwc/updateButton/updateButton.js
+++ force-app/main/default/lwc/updateButton/updateButton.js
@@ -1,20 +1,8 @@
 import { LightningElement, wire } from 'lwc';
-import UpsertAdvisors from '@salesforce/apex/ucl_UpsertAdvisors.UpsertAdvisors';
-import UpsertClients from '@salesforce/apex/uclUpsertClients.UpsertClients';
+import UpsertClients from '@salesforce/apex/ucl_UpsertClients.UpsertClients';
 
 export default class updateButton extends LightningElement {
 
-    @wire (UpsertAdvisors)
-    wiredUpsertAdvisors({error, data}){
-        if(data){
-            this.legalAdvisor = data;
-            alert('Lo hizo Bien');
-        }else if(error){
-            this.legalAdvisor = error;
-            alert('Lo hizo Mal' + error);
-            console.log(error)
-        }
-    }
     @wire (UpsertClients)
     wiredUpsertAdvisors({error, data}){
         if(data){
@@ -27,7 +15,6 @@ export default class updateButton extends LightningElement {
         }
     }
     handleClick(){
-        UpsertAdvisors();
         UpsertClients();
         alert('El button funciona')
     }
